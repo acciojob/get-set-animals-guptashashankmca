5137b710dc0eca23b5c659f70644aa9329919721
guptashashankmca <165798455+guptashashankmca@users.noreply.github.com>
2024-08-24 14:30:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,93 @@
-//complete this code
+// //complete this code
+// class Animal {
+// 	constructor(species){
+// 		this.species = species;
+// 	}
+// 	get species(){
+// 		return this.species;
+// 	}
+// 	makeSound(){
+// 		console.log(`this ${this.species} make a sound`);
+// 	}
+// }
+// class Dog extends Animal {
+// 	bard(){
+// 		console.log('woof');
+// 	}
+// }
+
+// class Cat extends Animal {
+// 	purr(){
+// 		console.log('purr');
+// 	}
+// }
+
+// // Do not change the code below this line
+// window.Animal = Animal;
+// window.Dog = Dog;
+// window.Cat = Cat;
+
+
 class Animal {
-	constructor(species){
-		this.species = species;
-	}
-	get species(){
-		return this.species;
-	}
-	makeSound(){
-		console.log(`this ${this.species} make a sound`);
-	}
+  constructor(species) {
+    this._species = species; // Use a private property for internal storage
+  }
+
+  // Getter for species
+  get species() {
+    return this._species;
+  }
+
+  // Method to log the sound the animal makes
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
 }
+
 class Dog extends Animal {
-	bard(){
-		console.log('woof');
-	}
+  // Method to log "woof"
+  bark() {
+    console.log('woof');
+  }
 }
 
 class Cat extends Animal {
-	purr(){
-		console.log('purr');
-	}
+  // Method to log "purr"
+  purr() {
+    console.log('purr');
+  }
 }
 
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
